4475910c52925b33002fb51c27821b73e19b2bd2
A3Tom <Tom_Esq@Yahoo.com>
2020-12-01 20:46:43 +0000

    Added .Net5 project. Added config builder and appsettings config. Added top level calls base code because why not do it as a wee cheeky top level bugger, this feels so alien using top level C# calls. What a topper of a feeling, cheers for being weird Microsoft

---
+++ GeezYerScreensavers.Net5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+const string SAVE_TARGET_DIR_KEY = "ConfigLocations:SaveImageToLocation";
+const string SPOTLIGHT_IMAGE_DIR_KEY = "ConfigLocations:SpotlightImageLocation";
+
+IConfigurationRoot _config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+    .AddJsonFile("appsettings.json", false)
+    .Build();
+
+var saveTargetDir = Environment.ExpandEnvironmentVariables(_config.GetSection(SAVE_TARGET_DIR_KEY).Value);
+var spotlightImageDir = Environment.ExpandEnvironmentVariables(_config.GetSection(SPOTLIGHT_IMAGE_DIR_KEY).Value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("Remo");
+
+Console.Read();
